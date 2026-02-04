8360f5ae4d10b48623011128729d033d40049111
Luis-ucc <luis.escobara@campusucc.edu.co>
2026-02-04 22:13:40 +0000

    ejercicio

--- clase 1/arreglos.java
+++ clase 1/arreglos.java
@@ -16,6 +16,20 @@ public class arreglos {
 
         System.out.println("la suma de todos los elementos del arreglo [a] es: " + suma);
 
+        int sumapares = 0;
+        int sumaimpares = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                sumapares += a[i];
+            } else {
+                sumaimpares += a[i];
+            }
+        }
+
+        System.out.println("suma de pares " + sumapares);
+        System.out.println("suma de impares " + sumaimpares);
+
 
     }
 }
