44afa97031cf1d70515cf263aef749f099b18306
Luis-ucc <luis.escobara@campusucc.edu.co>
2026-02-04 20:39:13 +0000

    arreglo

---
+++ clase 1/arreglos.java
@@ -0,0 +1,12 @@
+public class arreglos {
+    public static void main(String[] args) throws Exception {
+
+        int[] a = {2, 6, 8, 1, 20, 40, 7, 3, 5};
+
+        System.out.println("tamaño del arreglo a: " + a.length);
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+        }
+    }
+}
