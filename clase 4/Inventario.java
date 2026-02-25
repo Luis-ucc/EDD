51f68f64e3a5758b75907987561c0168b251365a
Luis-ucc <luis.escobara@campusucc.edu.co>
2026-02-25 21:32:34 +0000

    creacion del contructor, get and set, y el arreglo

---
+++ clase 4/Inventario.java
@@ -0,0 +1,19 @@
+public class Inventario {
+    
+    public static void main(String[] args) {
+        
+        producto[] l = new producto[10];
+
+        l[0] = new producto(104, "sal", 2.500, 10);
+        l[1] = new producto(105, "panela", 2.300, 15);
+        l[2] = new producto(106, "azucar", 9.000, 20);
+        l[3] = new producto(107, "cereal", 12.000, 30);
+        l[4] = new producto(108, "harina", 4.000, 40);
+        l[5] = new producto(109, "aceite", 15.000, 15);
+        l[6] = new producto(110, "mantequilla", 12.000, 15);
+        l[7] = new producto(111, "leche", 5.200, 25);
+        l[8] = new producto(112, "arroz", 2.000, 12);
+        l[9] = new producto(113, "frijoles", 2.500, 11);
+
+    }
+}
